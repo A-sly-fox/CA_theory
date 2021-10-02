f41e2396a494923984df50655726b53f86a3f9a3
Hu Lihang <1291371@qq.com>
2021-10-02 20:15:43 +0800

    chapter2 sizeof each data

--- ch2_code/sizeof.c
+++ ch2_code/sizeof.c
@@ -5,5 +5,8 @@ int main(){
     printf("size of short:%dB\n",sizeof(short));
     printf("size of int:%dB\n",sizeof(int));
     printf("size of long:%dB\n",sizeof(long));
+    printf("size of long long:%dB\n",sizeof(long long));
+    printf("size of float:%dB\n",sizeof(float));
+    printf("size of double:%dB\n",sizeof(double));
     return 0;
 }
