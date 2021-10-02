37d38302fb4ada4e1a282d00183e38f70384c2b0
Hu Lihang <1291371@qq.com>
2021-10-02 20:13:20 +0800

    chapter2 sizeof each data

---
+++ ch2_code/sizeof.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    printf("size of char:%dB\n",sizeof(char));
+    printf("size of short:%dB\n",sizeof(short));
+    printf("size of int:%dB\n",sizeof(int));
+    printf("size of long:%dB\n",sizeof(long));
+    return 0;
+}
